b2264f85f2bbabe028d0a2b1e34d75566d143bae
Felipe Jdanov <profissionalfelipe@gmail.com>
2023-07-03 18:20:28 -0400

    Some questions still pending to answer, as well as review, conclusion

--- Starter_Files/queries.sql
+++ Starter_Files/queries.sql
@@ -1,18 +1,39 @@
--- loading data for card holder 2 and 18 from the database
-select t.*, c.*
-from transaction t 
-join credit_card c on t.card = c.card
-where c.cardholder_id in (2,18);
+-- How can you isolate (or group) the transactions of each cardholder?
+SELECT c.cardholder_id, t.id
+FROM transaction t 
+JOIN credit_card c ON t.card = c.card
+GROUP BY c.cardholder_id, t.id
+ORDER BY c.cardholder_id;
 
--- loading data of daily transactions from jan to jun 2018 for card holder 25
-select t.date, t.amount
-from transaction t 
-join credit_card c on t.card = c.card
-where c.cardholder_id = 25 and 
-	t.date BETWEEN '2018-01-01' AND '2018-07-01';
-	
-	
-select t.* from transaction t
-from transaction t 
-join credit_card c on t.card = c.card
-where c.cardholder_id = ?;
+-- Count the transactions that are less than $2.00 per cardholder.
+SELECT c.cardholder_id,	count(t.id) AS transctions_count
+FROM transaction t 
+JOIN credit_card c ON t.card = c.card
+WHERE t.amount < 2
+GROUP BY c.cardholder_id
+ORDER BY c.cardholder_id;
+
+-- Is there any evidence to suggest that a credit card has been hacked? Explain your rationale.
+-- Yes, there are several transactions where the number of decimal places is greater than two.
+
+-- What are the top 100 highest transactions made between 7:00 am and 9:00 am?
+SELECT c.cardholder_id, t.*
+FROM transaction t 
+JOIN credit_card c ON t.card = c.card
+WHERE cast(t.date::timestamp as time) > '7:00:00' 
+	AND cast(t.date::timestamp as time) < '9:00:00'
+LIMIT 100;
+
+
+-- Do you see any anomalous transactions that could be fraudulent?
+-- Yes, all transactions with more than two decimal places
+
+-- Is there a higher number of fraudulent transactions made during this time frame versus the rest of the day?
+
+-- Is there a higher number of fraudulent transactions made during this time frame versus the rest of the day?
+
+-- If you answered yes to the previous question, explain why you think there might be fraudulent transactions during this time frame.
+
+-- What are the top 5 merchants prone to being hacked using small transactions?
+
+-- Create a view for each of your queries.
